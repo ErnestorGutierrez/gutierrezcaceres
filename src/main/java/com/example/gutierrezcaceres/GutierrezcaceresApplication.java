59004db37e5bdf2d49a537b661bd8caf54a5ec35
ErnestorGutierrez <nestor_hp10@outlook.es>
2023-04-24 17:15:42 -0500

    first commit

---
+++ src/main/java/com/example/gutierrezcaceres/GutierrezcaceresApplication.java
@@ -0,0 +1,13 @@
+package com.example.gutierrezcaceres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutierrezcaceresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutierrezcaceresApplication.class, args);
+	}
+
+}
